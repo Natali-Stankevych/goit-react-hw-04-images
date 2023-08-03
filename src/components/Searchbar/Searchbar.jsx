874dd8748411c95ebbb9f334857456a58243c1c5
Natali-Stankevych <rusakovanatali94@gmail.com>
2023-08-03 20:03:33 +0300

    add hw

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,54 +1,48 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
-import { ImSearch } from 'react-icons/im';
+
 
 import { SearchbarHeader, Form, Button, Input } from './Searchbar.module';
 
-export default class Searchbar extends Component {
-  state = {
-    searchQuery: ``,
+export const Searchbar = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleQueryChange = ({ currentTarget: { value } }) => {
+    setSearchQuery(value.toLowerCase());
   };
 
-  handleQueryChange = ({ currentTarget: { value } }) => {
-    this.setState({ searchQuery: value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
-    const searchQuery = this.state.searchQuery.trim();
+  const handleSubmit = e => {
     e.preventDefault();
+    const trimSearchQuery = searchQuery.trim();
 
-    if (searchQuery.trim() === '') {
+    if (trimSearchQuery === '') {
       toast.info('Please, enter search word!');
       return;
     }
 
-    this.props.onSubmit(searchQuery);
-    this.setState({ searchQuery: '' });
+    onSubmit(trimSearchQuery);
+    setSearchQuery('');
   };
 
-  render() {
-    const { searchQuery } = this.state;
-    return (
-      <SearchbarHeader className="searchbar">
-        <Form className="form" onSubmit={this.handleSubmit}>
-              <Button type="submit" className="button">
-            <span className="button-label">   <ImSearch size={25} /></span>
-          </Button>
-          <Input
-            className="input"
-            type="text"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="searchQuery"
-            value={searchQuery}
-            onChange={this.handleQueryChange}
-          />
+  return (
+    <SearchbarHeader className="searchbar">
+      <Form className="form" onSubmit={handleSubmit}>
+        <Input
+          className="input"
+          type="text"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="searchQuery"
+          value={searchQuery}
+          onChange={handleQueryChange}
+        />
 
-      
-        </Form>
-      </SearchbarHeader>
-    );
-  }
-}
+        <Button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </Button>
+      </Form>
+    </SearchbarHeader>
+  );
+};
